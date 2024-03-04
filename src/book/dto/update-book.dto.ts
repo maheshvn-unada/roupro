de7e9e7533d2707cfb60812c7a95378f40a3484c
unknown <mahesh.vn@unada.io>
2024-03-04 16:53:29 +0530

    initial commit

---
+++ src/book/dto/update-book.dto.ts
@@ -0,0 +1,26 @@
+
+import {IsOptional, IsString, IsNumber, IsEnum} from 'class-validator'
+import {Category} from '../schemas/book.schema'
+
+
+export class UpdateBookDto{
+    @IsOptional()
+    @IsString()
+    readonly title: string
+
+    @IsOptional()
+    @IsString()
+    readonly description: string
+
+    @IsOptional()
+    @IsString()
+    readonly author: string
+
+    @IsOptional()
+    @IsNumber()
+    readonly price: number
+
+    @IsOptional()
+    @IsEnum(Category, {message: 'Please update correct category.'})
+    readonly category: Category
+}
